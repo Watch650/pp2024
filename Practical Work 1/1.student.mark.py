35187e01f66bc001c03d817ecbef817913df9d15
Watch650 <famicanxi4life@gmail.com>
2024-01-04 16:31:46 +0700

    Practical work 1

--- Practical Work 1/1.student.mark.py
+++ Practical Work 1/1.student.mark.py
@@ -1,7 +1,8 @@
+# Create dictionaries to store students and courses info
 students = []
 courses = {}
 
-# Input number of students in a class & their id, name, DoB
+# Input number of students in a class & info: id, name, DoB
 def input_students():
     num_students = int(input("Enter the number of students: "))
     for _ in range(num_students):
@@ -10,7 +11,7 @@ def input_students():
         dob = input("Enter student date of birth: \n")
         students.append((id, name, dob, {}))
 
-# Input number of courses
+# Input number of courses & info: id, name
 def input_courses():
     num_courses = int(input("Enter the number of courses: "))
     for _ in range(num_courses):
@@ -18,7 +19,7 @@ def input_courses():
         name = input("Enter course name: \n")
         courses[id] = name
 
-# Input course info: id, name
+# Input marks for student in this course
 def input_marks():
     student_id = int(input("Enter student ID: "))
     course_id = (input("Enter course ID: "))
